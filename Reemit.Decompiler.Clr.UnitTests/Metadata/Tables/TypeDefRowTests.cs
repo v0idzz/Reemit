0317be99e41b3b95c01ce36f01ad54fbb28bed97
Piotr Romanowski <piotr@romanowski.me>
2024-05-30 15:24:42 +0200

    Add tests for reading metadata table rows that were left uncovered by tests

---
+++ Reemit.Decompiler.Clr.UnitTests/Metadata/Tables/TypeDefRowTests.cs
@@ -0,0 +1,34 @@
+using Reemit.Decompiler.Clr.Metadata;
+using Reemit.Decompiler.Clr.Metadata.Tables;
+
+namespace Reemit.Decompiler.Clr.UnitTests.Metadata.Tables;
+
+public class TypeDefRowTests
+{
+    [Fact]
+    public async Task Constructor_ValidTypeDefRow_ReadsTypeDefRow()
+    {
+        // Arrange
+        byte[] bytes = [0x01, 0x00, 0x10, 0x00, 0x01, 0x00, 0xEB, 0x01, 0x35, 0x00, 0x01, 0x00, 0x01, 0x00];
+        await using var memoryStream = new MemoryStream(bytes);
+        using var reader = new BinaryReader(memoryStream);
+        var row = new TypeDefRow();
+
+        // Act
+        row.Read(new MetadataTableDataReader(reader, 0, new Dictionary<MetadataTableName, uint>
+        {
+            { MetadataTableName.TypeRef, 1 },
+            { MetadataTableName.Field, 1 },
+            { MetadataTableName.MethodDef, 1 }
+        }));
+        
+        // Assert
+        Assert.Equal(TypeAttributes.Public, row.Flags);
+        Assert.Equal(1u, row.TypeName);
+        Assert.Equal(491u, row.TypeNamespace);
+        Assert.Equal(MetadataTableName.TypeRef, row.Extends.ReferencedTable);
+        Assert.Equal(13u, row.Extends.Rid);
+        Assert.Equal(1u, row.FieldList);
+        Assert.Equal(1u, row.MethodList);
+    }
+}
---
+++ Reemit.Decompiler.Clr.UnitTests/Metadata/Tables/TypeRefRowTests.cs
@@ -0,0 +1,29 @@
+using Reemit.Decompiler.Clr.Metadata;
+using Reemit.Decompiler.Clr.Metadata.Tables;
+
+namespace Reemit.Decompiler.Clr.UnitTests.Metadata.Tables;
+
+public class TypeRefRowTests
+{
+    [Fact]
+    public async Task Constructor_ValidTypeRefRow_ReadsTypeRefRow()
+    {
+        // Arrange
+        byte[] bytes = [0x06, 0x00, 0x2B, 0x00, 0xA2, 0x01];
+        await using var memoryStream = new MemoryStream(bytes);
+        using var reader = new BinaryReader(memoryStream);
+        var row = new TypeRefRow();
+
+        // Act
+        row.Read(new MetadataTableDataReader(reader, 0, new Dictionary<MetadataTableName, uint>
+        {
+            { MetadataTableName.AssemblyRef, 1 }
+        }));
+        
+        // Assert
+        Assert.Equal(MetadataTableName.AssemblyRef, row.ResolutionScope.ReferencedTable);
+        Assert.Equal(1u, row.ResolutionScope.Rid);
+        Assert.Equal(43u, row.TypeName);
+        Assert.Equal(418u, row.TypeNamespace);
+    }
+}
