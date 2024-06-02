6cacd974f63f583570de37c35d04d925b1c6fc17
John Douglas Leitch <johnleitch@outlook.com>
2024-06-02 12:20:37 -0400

    MethodDef updates.

--- Reemit.Decompiler.Clr/Metadata/Tables/MethodDefRow.cs
+++ Reemit.Decompiler.Clr/Metadata/Tables/MethodDefRow.cs
@@ -4,8 +4,8 @@ namespace Reemit.Decompiler.Clr.Metadata.Tables;
 
 public class MethodDefRow(
     uint rva,
-    MethodImplAttributes implFlags,
-    MethodAttributes flags,
+    ushort implFlags,
+    ushort flags,
     uint name,
     uint signature,
     uint paramList) : IMetadataTableRow<MethodDefRow>
@@ -13,31 +13,59 @@ public class MethodDefRow(
     public static MetadataTableName TableName => MetadataTableName.MethodDef;
 
     public uint Rva { get; } = rva;
-    public MethodImplAttributes ImplFlags { get; } = implFlags;
-    public MethodAttributes Flags { get; } = flags;
+    public ushort ImplFlags { get; } = implFlags;
+    public ushort Flags { get; } = flags;
     public uint Name { get; } = name;
     public uint Signature { get; } = signature;
     public uint ParamList { get; } = paramList;
 
+    public MethodImplCodeTypeAttributes CodeType =>
+        (MethodImplCodeTypeAttributes)(ImplFlags & (ushort)MethodImplCodeTypeAttributes.Mask);
+
+    public MethodImplManagedAttributes Managed =>
+        (MethodImplManagedAttributes)(ImplFlags & (ushort)MethodImplManagedAttributes.Mask);
+
+    public MethodImplAttributes MethodImpl =>
+        (MethodImplAttributes)(ImplFlags & (ushort)MethodImplAttributes.Mask);
+
+    public MethodMemberAccessAttributes MethodMemberAccess =>
+        (MethodMemberAccessAttributes)(Flags & (ushort)MethodMemberAccessAttributes.Mask);
+
+    public MethodVtableLayoutAttributes MethodVtableLayout =>
+        (MethodVtableLayoutAttributes)(Flags & (ushort)MethodVtableLayoutAttributes.Mask);
+
+    public MethodAttributes MethodFlags =>
+        (MethodAttributes)(Flags & (ushort)MethodAttributes.Mask);
+
+    public bool IsStatic => (MethodFlags & MethodAttributes.Static) != 0;
+    public bool IsFinal => (MethodFlags & MethodAttributes.Final) != 0;
+    public bool IsVirtual => (MethodFlags & MethodAttributes.Virtual) != 0;
+    public bool IsHideBySig => (MethodFlags & MethodAttributes.HideBySig) != 0;
+    public bool IsStrict => (MethodFlags & MethodAttributes.Strict) != 0;
+    public bool IsAbstract => (MethodFlags & MethodAttributes.Abstract) != 0;
+    public bool IsSpecialName => (MethodFlags & MethodAttributes.SpecialName) != 0;
+    public bool IsPInvokeImpl => (MethodFlags & MethodAttributes.PInvokeImpl) != 0;
+    public bool IsUnmanagedExport => (MethodFlags & MethodAttributes.UnmanagedExport) != 0;
+    public bool IsRTSpecialName => (MethodFlags & MethodAttributes.RTSpecialName) != 0;
+    public bool HasSecurity => (MethodFlags & MethodAttributes.HasSecurity) != 0;
+    public bool RequireSecObject => (MethodFlags & MethodAttributes.RequireSecObject) != 0;
+
     public static MethodDefRow Read(MetadataTableDataReader reader)
     {
         var rva = reader.ReadUInt32();
-        
-        var implFlagsBits = reader.ReadUInt16();
-        var codeType = (MethodImplCodeTypeAttributes)(implFlagsBits & (ushort)MethodImplCodeTypeAttributes.Mask);
-        var managed = (MethodImplManagedAttributes)(implFlagsBits & (ushort)MethodImplManagedAttributes.Mask);
-        var methodImpl = (MethodImplManagedAttributes)(implFlagsBits & (ushort)MethodImplAttributes.Mask);
 
-        var flagsBits = reader.ReadUInt16();
-        var methodAccess = (MethodMemberAccessAttributes)(flagsBits & (ushort)MethodMemberAccessAttributes.Mask);
-        var vtableLayout = (MethodVtableLayoutAttributes)(flagsBits & (ushort)MethodVtableLayoutAttributes.Mask);
-        var memberAttributes = (MethodAttributes)(flagsBits & (ushort)MethodAttributes.Mask);
+        // Todo:
+        // Validate these by ORing all flags, then AND NOTing to verify
+        // no unexpected bits are set.
+        var implFlags = reader.ReadUInt16();
+        var flags = reader.ReadUInt16();
 
-
-        var name = reader.ReadStringRid();
-        var signature = reader.ReadBlobRid();
-        var paramList = reader.ReadRidIntoTable(MetadataTableName.Param);
-
-        return null!;
+        return new(
+            rva,
+            implFlags,
+            flags,
+            reader.ReadStringRid(),
+            reader.ReadBlobRid(),
+            reader.ReadRidIntoTable(MetadataTableName.Param));
     }
 }
