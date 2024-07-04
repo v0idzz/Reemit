32e105e767449193affb6069b82c8f36c09ac762
John Douglas Leitch <johnleitch@outlook.com>
2024-07-04 00:32:23 -0400

    Made VM members into locals.

--- Reemit.Gui/ViewModels/HomeViewModel.cs
+++ Reemit.Gui/ViewModels/HomeViewModel.cs
@@ -13,23 +13,19 @@ namespace Reemit.Gui.ViewModels;
 
 public class HomeViewModel : ReactiveObject, IRoutableViewModel
 {
-    private readonly ReadOnlyObservableCollection<ClrModule> _modules;
-    
     public ModuleExplorerTreeViewModel ModuleExplorerTreeViewModel { get; }
     public HexEditorViewModel HexEditorViewModel { get; }
 
-    private readonly SourceList<ClrModule> _modulesSource;
-
     public HomeViewModel(IScreen hostScreen, IReadOnlyList<ClrModule> modules)
     {
-        _modulesSource = new SourceList<ClrModule>();
-        _modulesSource.AddRange(modules);
-        _modulesSource.Connect()
-            .Bind(out _modules)
+        var modulesSource = new SourceList<ClrModule>();
+        modulesSource.AddRange(modules);
+        modulesSource.Connect()
+            .Bind(out var observableModules)
             .Subscribe();
 
         HostScreen = hostScreen;
-        ModuleExplorerTreeViewModel = new ModuleExplorerTreeViewModel(_modules);
+        ModuleExplorerTreeViewModel = new ModuleExplorerTreeViewModel(observableModules);
         HexEditorViewModel = new HexEditorViewModel();
 
         this.WhenAnyValue(x => x.ModuleExplorerTreeViewModel.SelectedNode)
