b7c46b80e7d1e6a4afe90eff8bb8838f2d1a1b9c
John Douglas Leitch <johnleitch@outlook.com>
2024-08-03 23:29:21 -0400

    Added WinRT flag.

--- Reemit.Decompiler.Clr/Metadata/TypeImplementationAttributes.cs
+++ Reemit.Decompiler.Clr/Metadata/TypeImplementationAttributes.cs
@@ -7,8 +7,10 @@ public enum TypeImplementationAttributes : ushort
 {
     Import = 0x1000,
     Serializable = 0x2000,
-    
+    WindowsRuntime = 0x4000,
+
     Mask = 
         Import |
-        Serializable
+        Serializable |
+        WindowsRuntime
 }
