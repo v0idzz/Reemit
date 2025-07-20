f087d794d6fcca8038e83338c126a17d19061c84
Piotr Romanowski <piotr@romanowski.me>
2025-07-20 14:32:15 +0200

    Generic method param crash "hackish" fix

--- Reemit.Disassembler/ClrTypeInfo.cs
+++ Reemit.Disassembler/ClrTypeInfo.cs
@@ -46,16 +46,52 @@ public class ClrTypeInfo
         GenericArguments = genericArguments;
         IsGenericType = true;
         Namespace = @namespace;
-        
-        var nameBuilder = new StringBuilder(name[..name.LastIndexOf('`')]);
-        nameBuilder.Append('<');
-        nameBuilder.Append(
-            string.Join(", ",
-                genericArguments
-                    .Select(typeInfo => typeInfo.AliasOrName)));
-        nameBuilder.Append('>');
 
-        Name = nameBuilder.ToString();
+        /*
+         * This is a hackish way to tell whether the type itself accepts generic type args or if the generic type args
+         * are inferred from the inheritance hierarchy of the class where the type is referenced.
+         *
+         * Consider this example:
+         *
+         * ```csharp
+         * class GenericClass<T>
+         * {
+         *     public delegate T GenericMethod(T c);
+         * }
+         *
+         * class B : GenericClass<int>
+         * {
+         *     public B(GenericMethod genericMethod) {}
+         * }
+         * ```
+         *
+         * Looking at the code above, we can tell the `GenericMethod genericMethod` .ctor() parameter has `int` as its
+         * generic type arg.
+         *
+         * The type for `GenericMethod genericMethod` is encoded as ELEMENT_TYPE_GENERICINST.
+         * This gives us the information about the type parameter being `int` even though we cannot write the parameter
+         * type as `GenericMethod<int>` in the example above as that would result in invalid C#.
+         *
+         * I am sure that checking whether the generic args need to be appended to the type name could be done in a better
+         * way than looking for the backtick, but this keeps us going for now.
+         */
+        var indexOfTick = name.LastIndexOf('`');
+        if (indexOfTick != -1)
+        {
+            var nameBuilder = new StringBuilder(name[..indexOfTick]);
+            nameBuilder.Append('<');
+            nameBuilder.Append(
+                string.Join(", ",
+                    genericArguments
+                        .Select(typeInfo => typeInfo.AliasOrName)));
+            nameBuilder.Append('>');
+
+            Name = nameBuilder.ToString();
+        }
+        else
+        {
+            Name = name;
+        }
     }
 
     [MemberNotNullWhen(true, nameof(GenericArguments))]
