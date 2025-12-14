bddc4ab22a4d508c5cf6624fcdc1d0b9aee384ee
Piotr Romanowski <piotr@romanowski.me>
2025-12-14 18:40:03 +0100

    Basic IL syntax highlighting

--- Reemit.Gui/App.axaml.cs
+++ Reemit.Gui/App.axaml.cs
@@ -1,7 +1,12 @@
+using System;
 using System.Reflection;
+using System.Xml;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform;
+using AvaloniaEdit.Highlighting;
+using AvaloniaEdit.Highlighting.Xshd;
 using ReactiveUI;
 using Reemit.Gui.ViewModels;
 using Reemit.Gui.Views;
@@ -31,8 +36,20 @@ public partial class App : Application
 
     public override void RegisterServices()
     {
+        RegisterILHighlightingTheme("ILLight");
+        RegisterILHighlightingTheme("ILDark");
+
         Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetExecutingAssembly());
 
         base.RegisterServices();
+    }
+
+    private void RegisterILHighlightingTheme(string themeName)
+    {
+        using var stream = AssetLoader.Open(new Uri($"avares://Reemit.Gui/Assets/{themeName}.xshd"));
+        using var reader = XmlReader.Create(stream);
+
+        var highlighting = HighlightingLoader.Load(reader, HighlightingManager.Instance);
+        HighlightingManager.Instance.RegisterHighlighting(themeName, [".il"], highlighting);
     }
 }
--- Reemit.Gui/Views/Controls/ILView/ILView.axaml.cs
+++ Reemit.Gui/Views/Controls/ILView/ILView.axaml.cs
@@ -1,5 +1,8 @@
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Avalonia;
 using Avalonia.ReactiveUI;
+using AvaloniaEdit.Highlighting;
 using ReactiveUI;
 using Reemit.Gui.ViewModels.Controls.ILView;
 
@@ -14,6 +17,14 @@ public partial class ILView : ReactiveUserControl<ILViewModel>
         this.WhenActivated(disposable =>
         {
             this.OneWayBind(ViewModel, vm => vm.ILCode, v => v.TextEditor.Text)
+                .DisposeWith(disposable);
+
+            Observable.FromEventPattern(handler => Application.Current!.ActualThemeVariantChanged += handler,
+                    handler => Application.Current!.ActualThemeVariantChanged -= handler)
+                .Select(_ => (string)Application.Current!.ActualThemeVariant.Key == "Dark")
+                .StartWith((string)Application.Current!.ActualThemeVariant.Key == "Dark")
+                .Select(isDark => HighlightingManager.Instance.GetDefinition(isDark ? "ILDark" : "ILLight"))
+                .BindTo(TextEditor, v => v.SyntaxHighlighting)
                 .DisposeWith(disposable);
         });
     }
